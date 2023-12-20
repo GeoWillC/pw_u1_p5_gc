1d8fdb38acb24930d62edb69819a86fe2049f130
unknown <gwconlago@uce.edu.ec>
2023-12-19 23:42:03 -0500

    Tabla de estudiantes

--- js/listaEstudiantes.js
+++ js/listaEstudiantes.js
@@ -1,42 +1,24 @@
 console.log("elementos Vue");
 console.log(Vue);
 
-const estudiantes=[{nombre:'George',apellido:'Conlago'},
-{nombre:'Juan',apellido:'Perez'},
-{nombre:'Marco',apellido:'Padilla'},
-{nombre:'Pedro',apellido:'Montalvo'},
-{nombre:'',apellido:'Velasquez'}]
-
-
-
+const estudiantes=[
+{nombre:'George',apellido:'Conlago',cedula:'1751242698',genero:'M',edad:'14'},
+{nombre:'Juan',apellido:'Perez',cedula:'1751242622',genero:'M',edad:'15'},
+{nombre:'',apellido:'Padilla',cedula:'1751242633',genero:'M',edad:'16'},
+{nombre:'Pedro',apellido:'Montalvo',cedula:'1751242611',genero:'M',edad:'17'},
+{nombre:'Maria',apellido:'Velasquez',cedula:'175124000',genero:'F',edad:'18'}]
 
 console.log(estudiantes)
 console.table(estudiantes)
 const app = Vue.createApp({
 
-    /*template: `
-     <h1>Hola Mundo</h1>
-     <h1>Desde App.js</h1>
-     <h1>{{1+1}}</h1>
-     <p>{{[1,2,3,4,5,6,7,8]}} </p>
-     <p>{{true?'Verdadero':'Falso'}} </p>
-     `*/
+
+    
     methods: {
-        CambiarMensaje(){
-            console.log("se esta cambiando el mensaje")
-            console.log(this.mensaje)
-            this.mensaje='Valor cambiado'
-        },
-        CambiarNumero(){
-            this.valor=this.valor+1
-            console.log(this.valor)
-        },
-        Restaurar(){
-            this.valor=100
-        },
-        AgregarEstudiante(){
+        agregarEstudiante(){
             console.log("Agregando estudiante.........")
-            const estu={nombre:this.nombre,apellido:this.apellido}
+            const estu={nombre:this.nombre,apellido:this.apellido,cedula:this.cedula,
+                genero:this.genero,edad:this.edad}
             // this.lista.unshift(estu)
             this.lista.push(estu)
             console.log(typeof(nombre))
@@ -52,12 +34,12 @@ const app = Vue.createApp({
         agregarApellido(event){
             if(event.charCode===13){
                 console.log("In")
-               this.AgregarEstudiante()
+               this.agregarEstudiante()
             }
         },
         agregarEstudianteMod({charCode}){
             console.log("Presionando 2........")
-            this.AgregarEstudiante()   
+            this.agregarEstudiante()   
         }
     },
     watch: {
@@ -65,11 +47,14 @@ const app = Vue.createApp({
     },
     data() {
         return {
-            estudiantes: [{nombre:'George',apellido:'Conlago'},
-            {nombre:'Juan',apellido:'Perez'},
-            {nombre:'Marco',apellido:'Padilla'},
-            {nombre:'Pedro',apellido:'Montalvo'},
-            {nombre:'',apellido:'Velasquez'}]
+            lista:estudiantes,
+            valor: 0,
+            nombre:'',
+            apellido:'',
+            cedula:'',
+            genero:'',
+            edad:''
+           
         }
     }
 })
